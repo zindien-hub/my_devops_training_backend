d02ca72912d503fb67542e832339756b89f69902
JR Jernival <267707087+zindien-hub@users.noreply.github.com>
2026-04-12 15:29:21 +0200

    feat: fix user authentication API and generate JWT

--- src/main/java/com/openclassrooms/etudiant/service/JwtService.java
+++ src/main/java/com/openclassrooms/etudiant/service/JwtService.java
@@ -1,14 +1,28 @@
 package com.openclassrooms.etudiant.service;
 
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 @Service
 public class JwtService {
 
-    public String generateToken(UserDetails userDetails) {
-        return null; // TODO
-    }
+    private static final String SECRET_KEY = "mySuperSecretKeyForJwtTokenGeneration123456";
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 heure
 
+    public String generateToken(UserDetails userDetails) {
+        SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
 }
