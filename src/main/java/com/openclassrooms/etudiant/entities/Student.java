8352500cfb7f74eb7d6b6527f3b538ae62082f4c
JR Jernival <267707087+zindien-hub@users.noreply.github.com>
2026-04-15 18:14:31 +0200

    Add Student entity for CRUD operations

---
+++ src/main/java/com/openclassrooms/etudiant/entities/Student.java
@@ -0,0 +1,31 @@
+package com.openclassrooms.etudiant.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "students")
+@Data
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
